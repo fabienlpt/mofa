2682d49750ced17132bbc88eb9e61f1d474f35d0
fabienlpt <lapertfabien@gmail.com>
2022-04-22 00:21:21 +0200

    creation des fonctions ajout et suppression d'un article et creation du template nouvel article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,5 +19,31 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'article' => $article
         ]);
+    }
+
+
+    #[Route('/new_article', name: 'add')]
+    public function add(Request $request, EntityManagerInterface $em): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article)->handleRequest($request);
+        
+        if($form -> isSubmitted() && $form->isValid()) {
+            $em->persist($article);
+            $em->flush();
+        }
+        return $this->render('article/new_article.html.twig', [
+            'form' => $form -> createView()
+        ]);
+    }
+
+
+    #[Route('/delete/{id}', name: 'article_delete')]
+    public function delete(Article $article, EntityManagerInterface $em): Response
+    {
+        $em -> remove($article);
+        $em -> flush();
+
+        return $this->redirectToRoute('app_homepage');
     }
 }
